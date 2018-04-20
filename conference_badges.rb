2cab5ba3dcbf20aac0af10eeb91eae4977eb82d8
Caio Sanches <ccampato@gmail.com>
2018-04-20 05:53:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,7 @@ end
 def assign_rooms(array)
   room_assingment = []
   array.each_with_index { |speaker,index|
-    room_number = index + 1
-    room_assingment << "Hello, #{speaker}! You'll be assigned to room #{room_number}!"
+    room_assingment << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   }
   room_assingment
 end
